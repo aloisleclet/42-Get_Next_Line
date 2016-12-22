59fa5eee4c28f4a9fe88780c27a1ce71f14f8375
Alois LECLET <aleclet@e1r7p3.42.fr>
2016-12-22 18:01:02 +0100

    first commit

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aleclet <aleclet@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/22 17:41:13 by aleclet           #+#    #+#             */
+/*   Updated: 2016/12/22 17:47:26 by aleclet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUF_SIZE 1
+#include <stdlib.c>
+
+int get_next_line(int const fd, char **line);
+
+#endif
